7ad7970d096efeff4cde72f4b411efe65a0a6e31
dagui <davaguir852@gmail.com>
2023-06-26 21:40:00 -0700

    finished assignment

---
+++ api-11-books/models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose 
+
+// SCHEMA
+const bookSchema = new Schema({
+    title: String,
+    description: String,
+    year: Number,
+    quantity: Number,
+    imageURL: String
+})
+
+// MODEL AND EXPORT 
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
